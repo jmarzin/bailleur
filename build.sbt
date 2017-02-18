9c3b2db1282cc524383451229341b5a63b9e971d
jmarzin-cp <jacques.marzin@free.fr>
2017-02-18 22:57:27 +0100

    samedi 18 février

--- build.sbt
+++ build.sbt
@@ -7,4 +7,4 @@ scalaVersion := "2.11.8"
 libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.4"
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
-    
+libraryDependencies += "org.scala-lang.modules" %% "scala-swing" % "1.0.2"
--- src/main/scala-2.11/bailleurApp.scala
+++ src/main/scala-2.11/bailleurApp.scala
@@ -1,6 +1,10 @@
-import java.io.File
+import java.io.{File, PrintWriter}
+import javax.swing.filechooser.FileNameExtensionFilter
 import com.github.tototoshi.csv.{CSVReader, CSVWriter, DefaultCSVFormat}
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
+import scala.swing.Dialog.Message
+import scala.swing.{Dialog, FileChooser, Swing}
 
 /**
   * Created by jacquesmarzin on 03/02/2017.
@@ -15,7 +19,7 @@ object bailleurApp extends App {
       val debutNom = s.indexOf("(")
       if (debutNom < 0) return (s,Stream())
       val finNom = if (s.indexOf(")") < 0) s.length else s.indexOf(")")
-      val rue = s.substring(debutNom + 1, finNom) + " " + s.substring(0, debutNom - 1) + (if(finNom == s.length) "" else s.substring(finNom + 1))
+      val rue = (s.substring(debutNom + 1, finNom) + " " + s.substring(0, debutNom - 1) + (if (finNom == s.length) "" else s.substring(finNom + 1))).replaceAll("' ", "'").trim
       val debut2 = rue.indexOf("(")
       if(debut2 < 0) return (rue, Stream())
       val fin2 = if (rue.indexOf(")") < 0) rue.length else rue.indexOf(")")
@@ -38,41 +42,128 @@ object bailleurApp extends App {
           case _ =>
         }
       }
-      (rue.substring(0,debut2-1), ensemble)
+      (rue.substring(0,debut2-1).trim, ensemble)
     }
   }
+  def choosePlainFile(title: String = ""): Option[File] = {
+    val filter = new FileNameExtensionFilter("Fichier csv","csv")
+    val chooser = new FileChooser(new File("."))
+    chooser.title = title
+    chooser.fileFilter = filter
+    val result = chooser.showOpenDialog(null)
+    if (result == FileChooser.Result.Approve) Some(chooser.selectedFile) else None
+  }
 
-  val readerBailleur = CSVReader.open(new File("bailleur.csv"))
+  def rechercheSipCommune(comm: String, reglesCommunes: List[List[String]]): Option[String] = {
+    var iCommune = 0
+    do {
+      val communeTable = tableCommunes.get(comm.replaceFirst(reglesCommunes(iCommune).head,reglesCommunes(iCommune)(1)))
+      if(communeTable.isDefined){
+        return communeTable
+      } else {
+        iCommune += 1
+      }
+    } while (iCommune < reglesCommunes.size)
+    None
+  }
+
+  def rechercheSipVoie(voie: String, reglesVoies: List[List[String]]): Option[List[(String,Stream[Int])]] = {
+    var iVoie = 0
+    do {
+      val rueTable = tableRues.get(voie.replaceFirst(reglesVoies(iVoie).head,reglesVoies(iVoie)(1)))
+      if(rueTable.isDefined){
+        return rueTable
+      } else {
+        iVoie += 1
+      }
+    } while (iVoie < reglesVoies.size)
+    None
+  }
+
+  def nouvelleRegle(objet: String,
+                           nom: String,
+                           regles: List[List[Any]],
+                           recherche: (String, List[List[String]]) => Option[String]) : (Option[Any],List[List[Any]]) = {
+    var retour: Option[String] = None
+    var nouvellesRegles = regles
+    do {
+      retour = Dialog.showInput(null, nom, "Saisie d'une règle "+objet, Message.Plain,
+        Swing.EmptyIcon, Nil, nom + "|")
+      if (retour.isDefined) {
+        var tableChaines = retour.get.split('|')
+        if(tableChaines.length == 1) tableChaines = Array(tableChaines.head,"")
+        if (tableChaines.length == 2) {
+          val sipCommune = recherche(nom, List(tableChaines.toList))
+          if (sipCommune.isDefined) {
+            retour = None
+            nouvellesRegles = nouvellesRegles :+ tableChaines.toList
+            writerRegles.writeRow(List(objet, tableChaines(0), tableChaines(1)))
+            writerRegles.flush()
+            return(sipCommune,nouvellesRegles)
+          }
+        }
+      }
+    } while (retour.isDefined)
+    (None,nouvellesRegles)
+  }
+  // lecture des paramètre
+
+
+  var listeFichiers = List[String]()
+  val fichierParam = "bailleur.par"
+  if(new File(fichierParam).exists()){
+    listeFichiers = Source.fromFile(fichierParam).getLines().toList
+  }
+
+  // initialisation du fichier du bailleur
+
+  val fichierBailleur = if(listeFichiers.isEmpty)
+    choosePlainFile("Fichier du bailleur")
+  else
+    Some(new File(listeFichiers.head))
+  if(fichierBailleur.isEmpty) System.exit(-1)
+  val readerBailleur = CSVReader.open(fichierBailleur.get)
   val bailleur = readerBailleur.all()
   val bailleurBuf = for(ligne<-bailleur) yield ligne.to[ListBuffer]
-  if(!bailleurBuf.head.contains("COMMUNE")){
-    println("le fichier ne contient pas de commune")
+  val indexCommuneBailleur = bailleurBuf.head.indexOf("COMMUNE")
+  if(indexCommuneBailleur < 0) {
+    println("le fichier ne contient pas de colonne COMMUNE")
+    System.exit(-1)
+  }
+  val indexVoie = bailleurBuf.head.indexOf("VOIE")
+  if(indexVoie < 0) {
+    println("le fichier ne contient pas de colonne VOIE")
     System.exit(-1)
   }
   if(bailleurBuf.head.contains("SIP")) {
     println("le fichier est déjà traité")
     System.exit(-1)
   }
-  //noinspection ZeroIndexToHead
   bailleurBuf(0) += "SIP"
 
-  val indexCommuneBailleur = bailleurBuf.head.indexOf("COMMUNE")
+  // initialisation du fichier des communes
 
-  val readerCommunes = CSVReader.open(new File("communes.csv"))
+  val fichierCommunes = if(listeFichiers.isEmpty)
+    choosePlainFile("Fichier des communes")
+  else
+    Some(new File(listeFichiers(1)))
+  if(fichierCommunes.isEmpty) System.exit(-1)
+  val readerCommunes = CSVReader.open(fichierCommunes.get)
   val communes = readerCommunes.all()
-
   val indexCommuneCommunes = communes.head.indexOf("COMMUNE")
   val indexSipCommunes = communes.head.indexOf("Service des Impôts des Particuliers (SIP)")
+  val tableCommunes = (for(c<-communes) yield c(indexCommuneCommunes) -> c(indexSipCommunes)).tail.toMap
 
-  val tableSip = (for(c<-communes) yield c(indexCommuneCommunes) -> c(indexSipCommunes)).tail.toMap
+  // initialisation du fichier des rues du chef-lieu
 
-  val indexRue = bailleurBuf.head.indexOf("RUE")
-
-  val readerRuesToulouse = CSVReader.open(new File("RUES DE TOULOUSE.csv"))
+  val fichierRues = if(listeFichiers.isEmpty)
+    choosePlainFile("Fichier des rues du chef-lieu")
+  else
+    Some(new File(listeFichiers(2)))
+  if(fichierRues.isEmpty) System.exit(-1)
+  val readerRuesToulouse = CSVReader.open(fichierRues.get)
   val rues = readerRuesToulouse.all()
-
   var tableRues: scala.collection.mutable.Map[String, List[(String,Stream[Int])]] = _
-
   for(i<- 1 until rues.size) {
     val rue = rues(i)(1).nomVoie
     if(tableRues == null) {
@@ -84,41 +175,87 @@ object bailleurApp extends App {
     }
   }
 
+  if(listeFichiers.isEmpty){
+    val pw = new PrintWriter("bailleur.par")
+    pw.write(fichierBailleur.get.getPath+"\n"+fichierCommunes.get.getPath+"\n"+fichierRues.get.getPath)
+    pw.close()
+  }
+  // initialisation du fichier des régles de remplacement
+
+  val nomFichierRegles = fichierBailleur.get.getPath.replace(".csv","_regles.csv")
+  val fichierRegles = new File(nomFichierRegles)
+  var reglesCommunes = List(List("",""))
+  var reglesVoies = List(List("",""))
+  var reglesNumeros = List(List("",""))
+  var writerRegles: CSVWriter = _
+  if(fichierRegles.exists()) {
+    val readerRegles = CSVReader.open(fichierRegles)
+    val regles = readerRegles.all.tail
+    reglesCommunes = reglesCommunes ++ regles.filter(_(0).toUpperCase=="COMMUNE").map(p => List(p(1),p(2)))
+    reglesVoies = reglesVoies ++ regles.filter(_(0).toUpperCase=="VOIE").map(p => List(p(1),p(2)))
+    reglesNumeros = reglesNumeros ++ regles.filter(_(0).toUpperCase=="NUMERO").map(p => List(p(1),p(2)))
+    writerRegles = CSVWriter.open(fichierRegles,append = true)
+  } else {
+    writerRegles = CSVWriter.open(fichierRegles,append = true)
+    writerRegles.writeRow(List("COLONNE","VALEUR BAILLEUR","VALEUR REFERENTIEL"))
+    writerRegles.flush()
+  }
   for(i <- 1 until bailleurBuf.size) {
-    val comm = bailleurBuf(i)(indexCommuneBailleur)
-    if(comm == "TOULOUSE") {
-      val subst = List(("",""),("IMPASSE DU ","IMP "),("IMPASSE ","IMP "),("CHEMIN ","CHEM "),("RUE DE ","RUE "),
-        ("FR.  DEVEZE","FRANCOIS DEVEZE"),("AVENUE ","AVE "),("BD DE L'","BLD DE L' "),("RUE ","RUE DE "),
-        ("MAURICE MELAT", "MAURICE MELA"),("IMPASSE DU GENERAL AUBUGEOIS","RUE DU GENERAL AUBUGEOIS"),
-        ("ALLEE ","ALLS "),("RUE DU ","RUE "))
-      var rueTrouvee = false
-      var irue = 0
-      do {
-        val rueTable = tableRues.get(bailleurBuf(i)(indexRue).replaceFirst(subst(irue)._1, subst(irue)._2))
-        if (rueTable.isDefined) {
-          val rueRef = rueTable.get
-          if (rueRef.size == 1) {
-            bailleurBuf(i) += rueRef.head._1
-          } else {
-            val rueRefs = rueRef.filter(_._2.contains(bailleurBuf(i)(indexRue - 1).toInt))
-            if(rueRefs.isEmpty) {
-              bailleurBuf(i) += "aLaMain"
-            } else {
-              bailleurBuf(i) += rueRefs.head._1
+    val commune = bailleurBuf(i)(indexCommuneBailleur).toUpperCase()
+    var sipCommune = rechercheSipCommune(commune, reglesCommunes)
+    if(sipCommune.isEmpty) {
+      val retour = nouvelleRegle("COMMUNE",commune,reglesCommunes,rechercheSipCommune)
+      sipCommune = retour._1.asInstanceOf[Option[String]]
+      reglesCommunes = retour._2.asInstanceOf[List[List[String]]]
+    }
+    if(sipCommune.isEmpty) bailleurBuf(i) += "communeInconnue"
+    else if (!sipCommune.get.contains("voir")) bailleurBuf(i) += sipCommune.get
+    else {
+      val ruechaine = bailleurBuf(i)(indexVoie).replaceAll("é","e").replaceAll("è","e").replaceAll("ç","c").toUpperCase
+      var sipVoie = rechercheSipVoie(ruechaine, reglesVoies)
+      if(sipVoie.isEmpty) {
+        var retour: Option[String] = None
+        do {
+          retour = Dialog.showInput(null, commune, "Saisie d'une règle VOIE", Message.Plain,
+            Swing.EmptyIcon, Nil, ruechaine + "|")
+          if (retour.isDefined) {
+            var tableChaines = retour.get.split('|')
+            if (tableChaines.length == 1) tableChaines = Array(tableChaines.head, "")
+            if (tableChaines.length == 2) {
+              sipVoie = rechercheSipVoie(ruechaine, List(tableChaines.toList))
+              if (sipVoie.isDefined) {
+                retour = None
+                reglesVoies = reglesVoies :+ tableChaines.toList
+                writerRegles.writeRow(List("VOIE", tableChaines(0), tableChaines(1)))
+                writerRegles.flush()
+              }
             }
           }
-          rueTrouvee = true
-        } else {
-          irue += 1
-        }
-      } while (!rueTrouvee && irue < subst.size)
-      if(!rueTrouvee) {
-        bailleurBuf(i) += "rueInconnue"
+        } while (retour.isDefined)
       }
-    } else {
-      bailleurBuf(i) += tableSip.getOrElse(comm,
-        tableSip.getOrElse(comm.replaceFirst("ST ", "SAINT-"),
-          tableSip.getOrElse(comm.replaceFirst(" FONSEGRIVES", ""), "communeInconnue")))
+      if(sipVoie.isEmpty) bailleurBuf(i) += "voieInconnue"
+      else if (sipVoie.get.size == 1) bailleurBuf(i) += sipVoie.get.head._1
+      else {
+        val numeroRueC = bailleurBuf(i)(indexVoie - 1)
+        var numeroRue = if (numeroRueC.matches("""\d+""")) numeroRueC.toInt else 0
+        if(numeroRue == 0) {
+          var iNumero = 0
+          var numeroTrouve = false
+          do {
+            if (ruechaine.matches(reglesNumeros(iNumero).head)) {
+              numeroRue = reglesNumeros(iNumero)(1).toInt
+              numeroTrouve = true
+            } else iNumero += 1
+          } while (!numeroTrouve && iNumero < reglesNumeros.size)
+        }
+        val rueRef = sipVoie.get
+        val rueRefs = rueRef.filter(_._2.contains(numeroRue))
+        if(rueRefs.isEmpty) {
+          bailleurBuf(i) += "aLaMain"
+        } else {
+          bailleurBuf(i) += rueRefs.head._1
+        }
+      }
     }
   }
   val entete = bailleurBuf.head
@@ -126,14 +263,24 @@ object bailleurApp extends App {
   val corps = bailleurBuf.tail.sortWith(_ (indexSipBailleur)< _(indexSipBailleur))
   var sip = ""
   var writerBailleur: CSVWriter = _
+  var supprimerALaMain = true
+  var supprimerCommuneInconnue = true
+  var supprimerVoieInconnue = true
   for(ligne<-corps) {
     if(ligne(indexSipBailleur)!=sip) {
       if(writerBailleur != null) writerBailleur.close()
       sip = ligne(indexSipBailleur)
+      if(sip == "aLaMain") supprimerALaMain = false
+      if(sip == "communeInconnue") supprimerCommuneInconnue = false
+      if(sip == "voieInconnue") supprimerVoieInconnue = false
       writerBailleur = CSVWriter.open(new File("bailleur_complete_" + sip +".csv"))
       writerBailleur.writeRow(entete)
     }
     writerBailleur.writeRow(ligne)
   }
   writerBailleur.close()
+  if (supprimerALaMain) new File("bailleur_complete_aLaMain.csv").delete()
+  if (supprimerCommuneInconnue) new File("bailleur_complete_communeInconnue.csv").delete()
+  if (supprimerVoieInconnue) new File("bailleur_complete_voieInconnue.csv").delete()
+  if (supprimerCommuneInconnue && supprimerVoieInconnue && supprimerALaMain) new File("bailleur.par").delete()
 }
